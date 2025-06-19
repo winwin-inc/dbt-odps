e1540e784c02c8a0e32b4b8892a2a42edd6c7f02
lizhongshan <lizhongshan@brandct.com>
2025-06-19 09:57:45 +0800

    perf: cp37 as default python version

--- src/dbt/adapters/odps/context.py
+++ src/dbt/adapters/odps/context.py
@@ -11,4 +11,5 @@ GLOBAL_SQL_HINTS = {
     "odps.sql.allow.cartesian": "true",
     "odps.sql.timezone": "GMT",
     "odps.sql.allow.schema.evolution": "true",
+    "odps.sql.python.version": "cp37"
 }
