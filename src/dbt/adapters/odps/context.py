2c3807995f76bf17298a2a55fa3acb91b988ed96
lizhongshan <lizhongshan@brandct.com>
2025-07-02 14:39:14 +0800

    fix: gmt时区导致时区时间函数转换错误to_char

--- src/dbt/adapters/odps/context.py
+++ src/dbt/adapters/odps/context.py
@@ -9,7 +9,6 @@ GLOBAL_SQL_HINTS = {
     "odps.sql.select.output.format": "csv",
     "odps.sql.submit.mode": "script",
     "odps.sql.allow.cartesian": "true",
-    "odps.sql.timezone": "GMT",
     "odps.sql.allow.schema.evolution": "true",
     "odps.sql.python.version": "cp37"
 }
