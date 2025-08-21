7eeaec9d17a8d46b8d298e2d93875ae1663a4c7c
lizhongshan <lizhongshan@brandct.com>
2025-08-21 16:48:46 +0800

    fix: sql 长任务打印日志, 避免长时间等待无响应
    * 兼容airflow 3.0.4 心跳错误

--- src/dbt/adapters/odps/connections.py
+++ src/dbt/adapters/odps/connections.py
@@ -25,6 +25,11 @@ class ODPSConnectionManager(SQLConnectionManager):
         credentials = connection.credentials
         o = credentials.odps()
         options.user_agent_pattern = "dbt-odps $pyodps_version $python_version"
+        options.verbose = True
+        options.verbose_log = logger.debug
+        options.progress_time_interval = 300
+        
+        
         hints = GLOBAL_SQL_HINTS.copy()
         if credentials.hints:
             hints.update(credentials.hints)
